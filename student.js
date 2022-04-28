83e105743957481ba3e4e49205072a16e464b621
ChelseyBommer <chelseybommer@BTO-CHELSEY.localdomain>
2022-04-28 11:47:12 +0200

    changed server.js

---
+++ student.js
@@ -0,0 +1,11 @@
+
+const student = {
+    name: "Chelsey",
+    age: 18,
+    hobby: "tekenen"
+}
+
+module.exports = student;
+
+const camelCase = require('camelcase');
+console.log(camelCase('foo-bar'));
