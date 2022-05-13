2a9208803a1d8f1fc4d662f25b872d884cfcefd4
ChelseyBommer <chelseybommer@BTO-CHELSEY.localdomain>
2022-05-13 13:38:36 +0200

    add dropdown to form

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,18 @@
 /* functie gebeurd wanneer alles geladen is */
 window.addEventListener('load', () => { 
     
+   /* Haalt de textfield weg */
+   document.getElementById("form1").style.display = "none";
+
+   /* Voegt de dropdown toe */
+   document.getElementById("dropdown").style.display = "block";
+   
     /* Haalt de textfield weg */
-    document.getElementById("form1").style.visibility = "hidden";
+    document.getElementById("form2").style.display = "none";
 
     /* Voegt de slider toe */
-    document.getElementById("slider").style.visibility = "visible";
+    document.getElementById("slider").style.display = "block";
+
+    
   });
 
-  
