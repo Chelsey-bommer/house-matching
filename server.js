d2d6cc9ef7abcf11aca44d1f5946decfa72144e6
ChelseyBommer <chelseybommer@BTO-CHELSEY.localdomain>
2022-05-18 11:12:32 +0200

    log coordinates

--- server.js
+++ server.js
@@ -34,53 +34,29 @@ client.connect(err => {
 });
 
 
-function geoFindMe() {
-
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: °, Longitude: ${longitude} °`;
-    }
-
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
-}
   
 
 
 
-/* Lijst van steden dichtbij API */
+
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/locations/33.832213-118.387099/nearbyCities?radius=100';
+
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': process.env.API_HOST,
-		'X-RapidAPI-Key': process.env.API_KEY 
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': process.env.API_HOST,
+    'X-RapidAPI-Key': process.env.API_KEY
+  }
 };
 
-fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${latitude}+${longitude} /nearbyCities?radius=100&minPopulation=50000&distanceUnit=km`, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+fetch(url, options)
+	.then(res => res.json())
+	.then(json => console.log(json))
+	.catch(err => console.error('error:' + err));
+
+
+
+
 
 
 
@@ -90,16 +66,18 @@ app.get('/filter', (req, res) => {
 }) 
 
 
+
 /* filter route POST */
 app.post('/resultaten', (req, res) => {
     console.log(req.body.stad);
     console.log(req.body.budget);
+
    
     
     
    res.render('pages/results', {
        stad: req.body.stad,
-       budget: req.body.budget
+       budget: req.body.budget,
    }); 
 })
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,62 @@
 
 /* functie gebeurd wanneer alles geladen is */
-if (typeof window !== 'undefined') {
-  window.addEventListener('load', () => { 
-      
-    /* Haalt de textfield weg */
-    document.getElementById("text1").style.display = "none";
 
-    /* Voegt de dropdown toe */
-    document.getElementById("huizen1").style.display = "block";
-    
-      /* Haalt de textfield weg */
-      document.getElementById("text2").style.display = "none";
 
-      /* Voegt de slider toe */
-      document.getElementById("slider").style.display = "block";
-    
-  });
+window.addEventListener('load', () => {
+
+  /* Haalt de textfield weg */
+  document.getElementById("text1").style.display = "none";
+
+  /* Voegt de dropdown toe */
+  document.getElementById("huizen1").style.display = "block";
+
+  /* Haalt de textfield weg */
+  document.getElementById("text2").style.display = "none";
+
+  /* Voegt de slider toe */
+  document.getElementById("slider").style.display = "block";
+
+});
+
+
+globalThis.latitude
+globalThis.longitude
+
+
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    latitude  = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    console.log(latitude, longitude);
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 
 }
 
 
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
 
 
@@ -27,4 +64,17 @@ if (typeof window !== 'undefined') {
 
 
 
-// to self:  'https://wft-geo-db.p.rapidapi.com/v1/geo/locations/' + latitude + '+' + longitude + '/nearbyCities?radius=100&minPopulation=45000&distanceUnit=km'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
