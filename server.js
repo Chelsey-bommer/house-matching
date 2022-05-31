6121198bc25472b6010b7d9e203ea663d6fd4592
ChelseyBommer <chelseybommer@BTO-CHELSEY.localdomain>
2022-05-31 14:43:51 +0200

    linted

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 /** Variabelen **/
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const fetch = require('node-fetch');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
-let db = null;
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const fetch = require('node-fetch')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const { ObjectId } = require('mongodb')
+let db = null
 
 /** Middleware **/
-app.use('/static', express.static('./static'));
-app.use('/css', express.static('./static/css'));
-app.use('/img', express.static('./static/img'));
-app.use('/js', express.static('./static/js'));
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use('/static', express.static('./static'))
+app.use('/css', express.static('./static/css'))
+app.use('/img', express.static('./static/img'))
+app.use('/js', express.static('./static/js'))
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 /* Connect met database */
 async function connectDB () {
@@ -28,46 +27,43 @@ async function connectDB () {
     process.env.DB_HOST +
     '/' +
     process.env.DB_NAME +
-    '?retryWrites=true&w=majority';
+    '?retryWrites=true&w=majority'
 
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1
-  });
+  })
 
   try {
-    await client.connect();
-    db = client.db(process.env.DB_NAME);
+    await client.connect()
+    db = client.db(process.env.DB_NAME)
   } catch (error) {
-    throw error;
+    throw error
   }
 }
-
 
 /** ROUTES **/
 
 /** Home route **/
 app.get('/', (req, res) => {
-  res.render('pages/index');
-});
-
+  res.render('pages/index')
+})
 
 /** Filter route **/
 app.get('/filter', (req, res) => {
-  res.render('pages/filter');
-});
-
+  res.render('pages/filter')
+})
 
 /** Filter route POST **/
 app.post('/resultaten', async (req, res) => {
   /** Maak variabelen  **/
-  const stad = req.body.stad || req.body.textfield1;
-  const budgetString = req.body.budget;
-  const budget = Number(budgetString);
+  const stad = req.body.stad || req.body.textfield1
+  const budgetString = req.body.budget
+  const budget = Number(budgetString)
 
   /** Stuur userdata naar db  **/
-  await db.collection('user').insertOne({ stad, budget }, {});
+  await db.collection('user').insertOne({ stad, budget }, {})
 
   /** Haal huizen op uit db: namen**/
   const dbHouses = await db
@@ -75,11 +71,11 @@ app.post('/resultaten', async (req, res) => {
     .findOne(
       { $and: [{ stad }, { prijs: { $lte: budget } }] },
       { projection: { _id: 0, naam: 1 } }
-    );
-  let housesName = JSON.stringify(dbHouses);
-  housesName = housesName.replace(/[{}]/g, '');
-  housesName = housesName.replace(/[""]/g, '');
-  housesName = housesName.replace(/[":"]/g, ': ');
+    )
+  let housesName = JSON.stringify(dbHouses)
+  housesName = housesName.replace(/[{}]/g, '')
+  housesName = housesName.replace(/[""]/g, '')
+  housesName = housesName.replace(/[':']/g, ': ')
 
   /** Haal huizen op uit db: prijs **/
   const dbKosten = await db
@@ -87,11 +83,11 @@ app.post('/resultaten', async (req, res) => {
     .findOne(
       { $and: [{ stad }, { prijs: { $lte: budget } }] },
       { projection: { _id: 0, prijs: 1 } }
-    );
-  let housesKosten = JSON.stringify(dbKosten);
-  housesKosten = housesKosten.replace(/[{}]/g, '');
-  housesKosten = housesKosten.replace(/[""]/g, '');
-  housesKosten = housesKosten.replace(/[":"]/g, ': €');
+    )
+  let housesKosten = JSON.stringify(dbKosten)
+  housesKosten = housesKosten.replace(/[{}]/g, '')
+  housesKosten = housesKosten.replace(/[""]/g, '')
+  housesKosten = housesKosten.replace(/[':']/g, ': €')
 
   /** Haal huizen op uit db: steden**/
   const dbSteden = await db
@@ -99,12 +95,12 @@ app.post('/resultaten', async (req, res) => {
     .findOne(
       { $and: [{ stad }, { prijs: { $lte: budget } }] },
       { projection: { _id: 0, stad: 1 } }
-    );
+    )
 
-  let housesStad = JSON.stringify(dbSteden);
-  housesStad = housesStad.replace(/[{}]/g, '');
-  housesStad = housesStad.replace(/[""]/g, '');
-  housesStad = housesStad.replace(/[":"]/g, ': ');
+  let housesStad = JSON.stringify(dbSteden)
+  housesStad = housesStad.replace(/[{}]/g, '')
+  housesStad = housesStad.replace(/[""]/g, '')
+  housesStad = housesStad.replace(/[':']/g, ': ')
 
   /** render pagina **/
   res.render('pages/results', {
@@ -113,60 +109,52 @@ app.post('/resultaten', async (req, res) => {
     housesName,
     housesKosten,
     housesStad
-  });
-});
-
+  })
+})
 
 /**  Update route GET **/
 app.get('/update', async (req, res) => {
-
   /** Haal user data uit db**/
   const current = await db
     .collection('user')
-    .findOne({}, { projection: { _id: 0 } });
-  let housesCurrent = JSON.stringify(current);
-  housesCurrent = housesCurrent.replace(/[{}]/g, '');
-  housesCurrent = housesCurrent.replace(/[""]/g, '');
-  housesCurrent = housesCurrent.replace(/[":"]/g, ': ');
-  housesCurrent = housesCurrent.replace(/[","]/g, ', ');
+    .findOne({}, { projection: { _id: 0 } })
+  let housesCurrent = JSON.stringify(current)
+  housesCurrent = housesCurrent.replace(/[{}]/g, '')
+  housesCurrent = housesCurrent.replace(/[""]/g, '')
+  housesCurrent = housesCurrent.replace(/[':']/g, ': ')
+  housesCurrent = housesCurrent.replace(/[',']/g, ', ')
 
   /** render pagina **/
   res.render('pages/update', {
     stad: req.body.stad || req.body.textfield1,
     budget: req.body.budget,
     housesCurrent
-  });
-});
-
+  })
+})
 
 /* Update route POST */
 app.post('/update', async (req, res) => {
-
   /** Haal user data op uit db **/
   const current = await db
     .collection('user')
-    .findOne({}, { projection: { _id: 0 } });
-  let housesCurrent = JSON.stringify(current);
-  housesCurrent = housesCurrent.replace(/[{}]/g, '');
-  housesCurrent = housesCurrent.replace(/[""]/g, '');
+    .findOne({}, { projection: { _id: 0 } })
+  let housesCurrent = JSON.stringify(current)
+  housesCurrent = housesCurrent.replace(/[{}]/g, '')
+  housesCurrent = housesCurrent.replace(/[""]/g, '')
 
   /** Render pagina **/
-  res.render('pages/update', {housesCurrent});
-});
-
+  res.render('pages/update', { housesCurrent })
+})
 
 /** Resultaten update route POST **/
 app.post('/updateresultaten', async (req, res) => {
   /** Maak variabelen  **/
-  const stad = req.body.stad || req.body.textfield1;
-  const budgetString = req.body.budget;
-  const budget = Number(budgetString);
+  const stad = req.body.stad || req.body.textfield1
+  const budgetString = req.body.budget
+  const budget = Number(budgetString)
 
   /** Update user data in db  **/
-  db.collection('user').updateMany(
-    {},
-    { $set: { stad, budget } }
-  );
+  db.collection('user').updateMany({}, { $set: { stad, budget } })
 
   /** Haal huizen op uit db: naam **/
   const dbHouses = await db
@@ -174,11 +162,11 @@ app.post('/updateresultaten', async (req, res) => {
     .findOne(
       { $and: [{ stad }, { prijs: { $lte: budget } }] },
       { projection: { _id: 0, naam: 1 } }
-    );
-  let housesName = JSON.stringify(dbHouses);
-  housesName = housesName.replace(/[{}]/g, '');
-  housesName = housesName.replace(/[""]/g, '');
-  housesName = housesName.replace(/[":"]/g, ': ');
+    )
+  let housesName = JSON.stringify(dbHouses)
+  housesName = housesName.replace(/[{}]/g, '')
+  housesName = housesName.replace(/[""]/g, '')
+  housesName = housesName.replace(/[':']/g, ': ')
 
   /** Haal huizen op uit db: prijs **/
   const dbKosten = await db
@@ -186,11 +174,11 @@ app.post('/updateresultaten', async (req, res) => {
     .findOne(
       { $and: [{ stad }, { prijs: { $lte: budget } }] },
       { projection: { _id: 0, prijs: 1 } }
-    );
-  let housesKosten = JSON.stringify(dbKosten);
-  housesKosten = housesKosten.replace(/[{}]/g, '');
-  housesKosten = housesKosten.replace(/[""]/g, '');
-  housesKosten = housesKosten.replace(/[":"]/g, ': €');
+    )
+  let housesKosten = JSON.stringify(dbKosten)
+  housesKosten = housesKosten.replace(/[{}]/g, '')
+  housesKosten = housesKosten.replace(/[""]/g, '')
+  housesKosten = housesKosten.replace(/[':']/g, ': €')
 
   /** Haal huizen op uit db: steden **/
   const dbSteden = await db
@@ -198,11 +186,11 @@ app.post('/updateresultaten', async (req, res) => {
     .findOne(
       { $and: [{ stad }, { prijs: { $lte: budget } }] },
       { projection: { _id: 0, stad: 1 } }
-    );
-  let housesStad = JSON.stringify(dbSteden);
-  housesStad = housesStad.replace(/[{}]/g, '');
-  housesStad = housesStad.replace(/[""]/g, '');
-  housesStad = housesStad.replace(/[":"]/g, ': ');
+    )
+  let housesStad = JSON.stringify(dbSteden)
+  housesStad = housesStad.replace(/[{}]/g, '')
+  housesStad = housesStad.replace(/[""]/g, '')
+  housesStad = housesStad.replace(/[':']/g, ': ')
 
   /** Render pagina **/
   res.render('pages/updateresultaten', {
@@ -211,19 +199,17 @@ app.post('/updateresultaten', async (req, res) => {
     housesName,
     housesKosten,
     housesStad
-  });
-});
-
+  })
+})
 
 /* 404 route */
 app.use(function (req, res) {
-  res.status(404).render('pages/error');
-});
-
+  res.status(404).render('pages/error')
+})
 
 /* Hier console log je met de variable port van hierboven */
 app.listen(process.env.PORT, () => {
-  console.log(`Webserver running on port localhost:${process.env.PORT}`);
+  console.log(`Webserver running on port localhost:${process.env.PORT}`)
 
-  connectDB().then(console.log('Connectie met database succesvol'));
-});
+  connectDB().then(console.log('Connectie met database succesvol'))
+})
--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,17 @@
 /** functie voor form visibility **/
 window.addEventListener('load', () => {
-  document.getElementById('text1').style.display = 'none';
-  document.getElementById('huizen1').style.display = 'block';
-});
-
+  document.getElementById('text1').style.display = 'none'
+  document.getElementById('huizen1').style.display = 'block'
+})
 
 /** Functie geeft dichtbije stedenlijst bij locatie **/
-function findLocation ()  {
+function findLocation () {
   function success (position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    let obj;
+    latitude = position.coords.latitude
+    longitude = position.coords.longitude
+    let obj
 
-    const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${latitude}+${longitude}/nearbyCities?radius=100&minPopulation=30000`;
+    const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${latitude}+${longitude}/nearbyCities?radius=100&minPopulation=30000`
 
     const options = {
       method: 'GET',
@@ -20,7 +19,7 @@ function findLocation ()  {
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
         'X-RapidAPI-Key': '873fdd2e2fmsh81540b4d55e8862p18db48jsnd50b25f4c0ca'
       }
-    };
+    }
 
     fetch(url, options)
       .then((response) => response.json())
@@ -28,52 +27,52 @@ function findLocation ()  {
       .then(() => console.log(obj.data))
 
       .then(() => {
-        const objec = String(obj.data[4].city);
-        const kop = document.getElementById('stad5');
-        kop.innerHTML = objec;
-        document.getElementById('stad5').value = objec;
+        const objec = String(obj.data[4].city)
+        const kop = document.getElementById('stad5')
+        kop.innerHTML = objec
+        document.getElementById('stad5').value = objec
       })
 
       .then(() => {
-        const objec = String(obj.data[3].city);
-        const kop = document.getElementById('stad4');
-        kop.innerHTML = objec;
-        document.getElementById('stad4').value = objec;
+        const objec = String(obj.data[3].city)
+        const kop = document.getElementById('stad4')
+        kop.innerHTML = objec
+        document.getElementById('stad4').value = objec
       })
 
       .then(() => {
-        const objec = String(obj.data[2].city);
-        const kop = document.getElementById('stad3');
-        kop.innerHTML = objec;
-        document.getElementById('stad3').value = objec;
+        const objec = String(obj.data[2].city)
+        const kop = document.getElementById('stad3')
+        kop.innerHTML = objec
+        document.getElementById('stad3').value = objec
       })
 
       .then(() => {
-        const objec = String(obj.data[1].city);
-        const kop = document.getElementById('stad2');
-        kop.innerHTML = objec;
-        document.getElementById('stad2').value = objec;
+        const objec = String(obj.data[1].city)
+        const kop = document.getElementById('stad2')
+        kop.innerHTML = objec
+        document.getElementById('stad2').value = objec
       })
 
       .finally(() => {
-        const objec = String(obj.data[0].city);
-        const kop = document.getElementById('stad1');
-        kop.innerHTML = objec;
-        document.getElementById('stad1').value = objec;
+        const objec = String(obj.data[0].city)
+        const kop = document.getElementById('stad1')
+        kop.innerHTML = objec
+        document.getElementById('stad1').value = objec
       })
 
-      .catch((err) => console.error('error:' + err));
+      .catch((err) => console.error('error:' + err))
   }
 
   function error () {
-    console.log('Unable to retrieve your location');
+    console.log('Unable to retrieve your location')
   }
 
   if (!navigator.geolocation) {
-    console.log('Geolocation is not supported by your browser');
+    console.log('Geolocation is not supported by your browser')
   } else {
-    navigator.geolocation.getCurrentPosition(success, error);
+    navigator.geolocation.getCurrentPosition(success, error)
   }
 }
 
-findLocation();
+findLocation()
