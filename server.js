ed72abbe8b75510b4040caef6cfc47c6654dd93a
ChelseyBommer <chelseybommer@BTO-CHELSEY.localdomain>
2022-06-16 11:35:47 +0200

    added: filter form error handling

--- server.js
+++ server.js
@@ -65,12 +65,11 @@ app.get('/filter', (req, res) => {
 
 /*** Filter route POST **/
 app.post('/resultaten', async (req, res) => {
-  
+
   /** Maak variabelen  **/
   const stad = req.body.stad || req.body.textfield1
   const budgetString = req.body.budget
   const budget = Number(budgetString)
-
 
   /** Stuur userdata naar db  **/
   await db.collection('user').insertOne({ stad, budget }, {})
@@ -87,13 +86,36 @@ app.post('/resultaten', async (req, res) => {
   houses = houses.replace(/[':']/g, ': ')
   houses = houses.replace(/[',']/g, ', ')
 
-  /** render pagina **/
-  res.render('pages/results', {
-    stad: req.body.stad || req.body.textfield1,
-    budget: req.body.budget,
-    houses
-  })
+  if (dbHouses == null) {
+    console.log('Dit bestaat niet')
+    res.render('pages/filter', {
+      
+    })
+
+  }
+  else {
+    /** Haal huizen op uit db**/
+    const dbHouses = await db
+      .collection('houses')
+      .findOne(
+        { $and: [{ stad }, { prijs: { $lte: budget } }] },
+        { projection: { _id: 0, naam: 1, prijs: 1, stad: 1 } }
+      )
+    let houses = JSON.stringify(dbHouses)
+    houses = houses.replace(/[{}]|[""]/g, '')
+    houses = houses.replace(/[':']/g, ': ')
+    houses = houses.replace(/[',']/g, ', ')
+
+    /** render pagina **/
+    res.render('pages/results', {
+      stad: req.body.stad || req.body.textfield1,
+      budget: req.body.budget,
+      houses
+    })
+  }
 })
+ 
+
 
 /**  Update route GET **/
 app.get('/update', async (req, res) => {
