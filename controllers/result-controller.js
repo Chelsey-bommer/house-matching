d4e2ed28f24c01d57cf59d2c5afe3b85f492b6c9
ChelseyBommer <chelseybommer@BTO-CHELSEY.localdomain>
2022-06-19 18:13:34 +0200

    updated: filter post function shorter&better

--- controllers/result-controller.js
+++ controllers/result-controller.js
@@ -6,65 +6,46 @@ const alert = require('alert')
 
 
 /*** Filter route POST **/
- router.post('/resultaten', async (req, res) => {
-
+router.post('/resultaten', async (req, res) => {
+ 
     /** Maak variabelen  **/
     const stad = req.body.stad || req.body.textfield1
     const budgetString = req.body.budget
     const budget = Number(budgetString)
-
+   
     /** Stuur userdata naar db  **/
     await db.collection('user').insertOne({ stad, budget }, {})
-
+   
     /** Haal huizen op uit db**/
-    const dbHouses = await db
-        .collection('houses')
-        .findOne(
-            { $and: [{ stad }, { prijs: { $lte: budget } }] },
-            { projection: { _id: 0, naam: 1, prijs: 1, stad: 1 } }
-        )
-    let houses = JSON.stringify(dbHouses)
-    houses = houses.replace(/[{}]|[""]/g, '')
-    houses = houses.replace(/[':']/g, ': ')
-    houses = houses.replace(/[',']/g, ', ')
-
+    const houses = await db
+      .collection('houses')
+      .findOne(
+        { $and: [{ stad }, { prijs: { $lte: budget } }]}
+      )
+   
+      console.log(houses);
+   
     try {
-        if (dbHouses != null) {
-            /** Haal huizen op uit db**/
-            const dbHouses = await db
-                .collection('houses')
-                .findOne(
-                    { $and: [{ stad }, { prijs: { $lte: budget } }] },
-                    { projection: { _id: 0, naam: 1, prijs: 1, stad: 1 } }
-                )
-            let houses = JSON.stringify(dbHouses)
-            houses = houses.replace(/[{}]|[""]/g, '')
-            houses = houses.replace(/[':']/g, ': ')
-            houses = houses.replace(/[',']/g, ', ')
-
-            /** render pagina **/
-            res.render('pages/results', {
-                stad: req.body.stad || req.body.textfield1,
-                budget: req.body.budget,
-                houses
-            })
-          
-        }
-        else {
-            alert('Dit huis bestaat niet, probeer andere voorkeuren')
-            alertHouses
-
-            /** render pagina **/
-            res.render('pages/filter', {
-                stad: req.body.stad || req.body.textfield1,
-                budget: req.body.budget,
-                houses
-            })
-        }
+      if (houses == null) {
+        alert('Dit huis bestaat niet, probeer andere voorkeuren')
+        alertHouses
+   
+        /** render pagina **/
+        res.render('pages/filter')
+      }
+      else {
+   
+        /** render pagina **/
+        res.render('pages/results', { 
+          stad: req.body.stad || req.body.textfield1,
+          budget: req.body.budget,
+          houses 
+        })
+      }
     }
-    catch {
-    
-    console.log("try something else")}
-})
+    catch (err) {
+      console.log(err)
+    }
+  })
 
 module.exports = router
